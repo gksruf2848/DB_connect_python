c5a02cd989e9ab558f5311688da22e941bc9c611
gksruf2848 <4455qqq@naver.com>
2022-08-31 11:51:32 +0900

    .

---
+++ sqlite3/HanlimSqlite3/HanlimSqlite_01_createdTable.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+db_filename = 'hanlim.db'
+
+with sqlite3.connect(db_filename, detect_types=sqlite3.PARSE_DECLTYPES) as conn:
+    conn.execute("""
+    create table if not exists obj (
+        id    integer primary key autoincrement not null,
+        data  text
+    )""")
+    
